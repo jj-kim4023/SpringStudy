59e874f8f639261d7fbd40c6cccfe190afcadfd6
jj-kim4023 <jj.kim4023@gmail.com>
2024-06-07 23:11:58 +0900

    [CHORE] 4~5강 -자바 코드로 스프링 빈 등록 -홈 화면 추가 -회원 웹 기능(등록,조회)

--- src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -1,12 +1,12 @@
 package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+
 public class MemoryMemberRepository implements MemberRepository {
-
-
 
     private static Map<Long, Member> store = new HashMap<>();
     private static Long sequence = 0L;
